6efb3a0eef8b748994757de0e007dd7f6d00fbbd
Alfio Martini <alfio.martini@gmail.com>
2020-06-30 16:26:36 -0300

    Updated database.py

--- database.py
+++ database.py
@@ -77,9 +77,11 @@ class MySQL(SQL):
 #mydb = MySQL(os.getenv("CS50FINAL_DB_URL"))
 # mydb = MySQL("sqlite:///database/cs50final.db")
 
-# For Heroku deployment
+# For Local use of Heroku 
 #mydb = MySQL("postgres://mtztrmkrqbfvgp:1260311e94b8d2608be808433512f8d34dc623526e61ad1f04c2e3336b3a0f8a@ec2-52-202-66-191.compute-1.amazonaws.com:5432/d30b48o0gtbpnp")
-mydb = MySQL("DATABASE_URL")
+
+# For Heroku deployment
+mydb = MySQL(os.getenv("DATABASE_URL"))
          
 # enables foreign key constraints at runtime
 #db.execute('PRAGMA foreign_keys = ON')
