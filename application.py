bb55c853998264985eda56d8afaa2f7aac88b824
Alfio Martini <alfio.martini@gmail.com>
2020-06-30 15:28:42 -0300

    Changed database for Heroku postgresql

--- application.py
+++ application.py
@@ -93,5 +93,5 @@ for code in default_exceptions:
     app.errorhandler(code)(errorhandler)
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    # app.run()
+    # app.run(debug=True)
+    app.run()
