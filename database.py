b7c8e182e0e2c281dd20e8353e3b8c2c44c05b2b
Alfio Martini <alfio.martini@gmail.com>
2020-06-30 16:20:14 -0300

    Updated database.py with Heroku's DATABASE_URL

--- database.py
+++ database.py
@@ -78,7 +78,8 @@ class MySQL(SQL):
 # mydb = MySQL("sqlite:///database/cs50final.db")
 
 # For Heroku deployment
-mydb = MySQL("postgres://mtztrmkrqbfvgp:1260311e94b8d2608be808433512f8d34dc623526e61ad1f04c2e3336b3a0f8a@ec2-52-202-66-191.compute-1.amazonaws.com:5432/d30b48o0gtbpnp")
+#mydb = MySQL("postgres://mtztrmkrqbfvgp:1260311e94b8d2608be808433512f8d34dc623526e61ad1f04c2e3336b3a0f8a@ec2-52-202-66-191.compute-1.amazonaws.com:5432/d30b48o0gtbpnp")
+mydb = MySQL("DATABASE_URL")
          
 # enables foreign key constraints at runtime
 #db.execute('PRAGMA foreign_keys = ON')
