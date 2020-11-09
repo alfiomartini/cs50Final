427978ad9dc227c79779c64f7060e61dc9a040eb
Alfio Martini <alfio.martini@gmail.com>
2020-11-09 20:30:41 -0300

    PostgreSQL URL made invisible

--- database.py
+++ database.py
@@ -5,13 +5,5 @@ import os
 
 mydb = SQL("sqlite:///database/cs50final.db")
 
-
-# For Local use of Heroku
-#mydb = MySQL("postgres://mtztrmkrqbfvgp:1260311e94b8d2608be808433512f8d34dc623526e61ad1f04c2e3336b3a0f8a@ec2-52-202-66-191.compute-1.amazonaws.com:5432/d30b48o0gtbpnp")
-
-# For Heroku deployment
-#mydb = SQL(os.getenv("DATABASE_URL"))
-
-
 # enables foreign key constraints at runtime
 # mydb.execute('PRAGMA foreign_keys = ON')
